af0ca4194637465bbd758b2f76f40b65c41ac054
Denis <denxen777@gmail.com>
2023-01-24 16:07:18 +0700

    Refactor

--- src/script.js
+++ src/script.js
@@ -4,8 +4,8 @@ const img = document.querySelector('.wrapper_image img');
 const prev = document.querySelector('.wrapper_image_prev');
 const next = document.querySelector('.wrapper_image_next');
 const wrapDots = document.querySelector('.wrapper_dots');
-const images = ['../src/images/Lam0.jpg','../src/images/Lam1.jpg','../src/images/Lam2.jpg','../src/images/Lam3.jpg',
-               '../src/images/Lam4.jpg','../src/images/Lam5.jpg','../src/images/Lam6.jpg','../src/images/Lam7.jpg'];
+const images = ['../src/images/Lam0.jpg', '../src/images/Lam1.jpg', '../src/images/Lam2.jpg', '../src/images/Lam3.jpg',
+  '../src/images/Lam4.jpg', '../src/images/Lam5.jpg', '../src/images/Lam6.jpg', '../src/images/Lam7.jpg'];
 
 
 for (let i = 0; i < images.length; i++) {
@@ -13,42 +13,25 @@ for (let i = 0; i < images.length; i++) {
   dot.classList.add('dot');
   wrapDots.appendChild(dot);
 }
+
 const dots = document.querySelectorAll('.dot');
-let count = images.indexOf('../src' + img.src.replace(/.*src/, ''));
-
+let count = 0;
+img.src = images[count];
 dots[count].classList.add('active');
-next.addEventListener('click', function (ev) {
+
+next.addEventListener('click', (ev) => monoton(ev, 1));
+prev.addEventListener('click', (ev) => monoton(ev, -1));
+
+const monoton = (ev, difference) => {
   ev.preventDefault();
-  count++;
-  if (count > images.length - 1) {
-    count = 0;
-  }
-
-  dots[count].classList.add('active');
-
-  if (count === 0) {
-    dots[images.length - 1].classList.remove('active');
-  } else {
-    dots[count - 1].classList.remove('active');
-  }
-
-  img.src = images[count];
-})
-
-prev.addEventListener('click', function (ev) {
-  ev.preventDefault();
-  count--;
+  dots[count].classList.remove('active');
+  count = count + difference;
   if (count < 0) {
     count = images.length - 1;
   }
-
-  dots[count].classList.add('active');
-
-  if (count === images.length - 1) {
-    dots[0].classList.remove('active');
-  } else {
-    dots[count + 1].classList.remove('active');
+  if (count > images.length - 1) {
+    count = 0;
   }
-
+  dots[count].classList.add('active');
   img.src = images[count];
-})
+}
